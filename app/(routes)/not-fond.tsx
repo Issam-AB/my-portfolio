4732d4b2291f274b463b14ec49f84a03f02cd8a5
unknown <issam.aboulfadl05@gmail.com>
2025-01-09 12:22:57 +0100

    Add initial blog, about, projects, and not found pages; implement global styles and button component

---
+++ app/(routes)/not-fond.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFond = () => {
+  return <div>NotFond</div>;
+};
+
+export default NotFond;
---
+++ app/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl underline font-bold text-red-600">Home Page</h1>
+    </div>
+  );
+}
