61ee14d514f6468f65357f9a164d6fcbfd3a1f29
Issam <issam.aboulfadl05@gmail.com>
2025-01-23 00:38:10 +0100

    feat: add resume link

--- components/ui/resume-btn.tsx
+++ components/ui/resume-btn.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { StarBorder } from "./star-border";
+import Link from "../link";
 
 const ResumeBtn = () => {
   return (
-    <StarBorder className="rounded-lg">
-      <span className="text-xs font-medium transition-colors text-muted-foreground hover:text-foreground">
-        Download CV
-      </span>
-    </StarBorder>
+    <Link
+      href="https://docs.google.com/document/d/1flCvDQ7Yfq9ososd3n37NnFaAOK40iV5Qv_AEulil-g/edit?usp=sharing"
+      target="_blank"
+    >
+      <StarBorder className="rounded-lg">
+        <span className="text-xs font-medium transition-colors text-muted-foreground hover:text-foreground">
+          Download CV
+        </span>
+      </StarBorder>
+    </Link>
   );
 };
 
