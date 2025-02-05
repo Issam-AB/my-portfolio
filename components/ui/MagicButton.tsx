59e15849bc16eed2adbdbea3f51a9023488c69c0
unknown <issam.aboulfadl05@gmail.com>
2025-02-05 12:15:59 +0100

    feat: update footer text and enhance MagicButton gradient; add scroll lock styles

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -26,7 +26,7 @@ const MagicButton = ({
       className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[2.5px] focus:outline-none outline-none"
       onClick={handleClick}
     >
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#FF3E29_0%,#ffc900_50%,#FF5223_80%)]" />
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#FF3E29_0%,#ffc900_60%,#FF5223_85%)]" />
 
       {/* remove px-3 py-1, add px-5 gap-2 */}
       <span
