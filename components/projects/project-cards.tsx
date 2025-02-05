38c302c1980f7fd4ace628a00b66dc5fc97b68b9
unknown <issam.aboulfadl05@gmail.com>
2025-02-05 14:06:28 +0100

    fix: adjust padding in project cards for improved layout and spacing consistency

--- components/projects/project-cards.tsx
+++ components/projects/project-cards.tsx
@@ -39,7 +39,7 @@ const ProjectCard = (props: ProjectCardProps) => {
         className="rounded-lg"
       />
 
-      <div className="px-2 sm:py-4 py-2">
+      <div className="px-2 sm:py-4 py-0">
         <div className="space-y-2">
           <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
             {name}
@@ -50,14 +50,11 @@ const ProjectCard = (props: ProjectCardProps) => {
           </p>
         </div>
 
-        <div className="mt-4 flex flex-wrap gap-2">
+        <div className="mt-4 flex flex-wrap gap-2 -space-x-4">
           {iconLists.map((iconPath, index) => (
             <div
               key={index}
-              className="border border-zinc-200 rounded-full lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center bg-[#F3F4F6] backdrop-blur-lg bg-opacity-30"
-              style={{
-                transform: `translateX(-${13 * index + 2}px)`,
-              }}
+              className="border border-zinc-200 dark:border-zinc-50/30 shrink-0 rounded-full lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center bg-[#F3F4F6] backdrop-blur-lg bg-opacity-30"
             >
               <TechIcon key={index} iconPath={iconPath} className="w-8 h-8" />
             </div>
